36b38ca35d1b3aece9ec93b7501360381c2de3f0
Daniel Mroczka <daniel.mroczka@gmail.com>
2026-03-04 07:13:20 +0100

    update default redirect page name to 'q_001.html'

--- gcp-devops-question/redirect.js
+++ gcp-devops-question/redirect.js
@@ -1,5 +1,5 @@
 (function() {
     const lastVisited = localStorage.getItem('gcp_devops_exam_last_visited');
-    const defaultPage = 'question_001.html';
+    const defaultPage = 'q_001.html';
     window.location.href = lastVisited ? lastVisited : defaultPage;
 })();
